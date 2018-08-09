7497e4e2528b03bcece57df9e887ec95fd7b3a4c
Deepesh-Kumar <42011384+Deepesh-Kumar@users.noreply.github.com>
2018-08-08 21:02:50 -0700

    Update upgrade_devices.py

--- upgrade_devices.py
+++ upgrade_devices.py
@@ -1,4 +1,12 @@
 
+#Upgrading Devices using vManage API call
+#This is a simple script for upgrading devices part of a particular overlay. We start with using a API call (GET Request) 
+# to fetch necessary data which includes device types and System IPs (Controllers or vEdge Devices) and use this information 
+# in the next API call(POST Request) to upgrade those devices.
+#The example shown here is done for hardware vEdge Devices, the script can be modifed to upgrade Controllers or software vEdge 
+#devices.
+
+
 import json
 import requests
 import itertools
@@ -7,103 +15,36 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 s = requests.session()
 
-#payload = {"action":"install","input":{"vEdgeVPN":0,"vSmartVPN":0,"data":[{"family":"vedge-x86","version":"18.3.0"}],"versionType":"vmanage","reboot":'true',"sync":'true'},
-#			"devices":[{"deviceIP":"1.1.1.15","deviceId":"5cbad91d-9805-441b-a27d-8d9f262cc788"}],"deviceType":"vedge"}
-#payload = json.dumps(payload)
-uri = 'https://10.195.168.110:8443/dataservice/device'
-response = s.get(uri, auth=HTTPBasicAuth('deepesh', 'deepesh4321!'), verify=False)
-d = response.json()
-#e = {}
-g = []
-h = []
-#print d['data']
+
+IP = raw_input("Enter the IP: ") .              # Enter the IP and credentials
+username = raw_input("Enter the username: ")
+password = raw_input("Enter the password:  ")
+
+
+uri = 'https://' + IP + '/dataservice/device'        # API Call for Fetching Data
+
+response = s.get(uri, auth=HTTPBasicAuth(username, password), verify=False)
+d = response.json()   # Storing the response 
+
+system_ip = []
+device_id = []
 for i in d['data']:
 	if i['personality'] == 'vedge' and i['reachability'] == 'reachable':
-		h.append(i['local-system-ip'])
-		g.append(i['uuid'])
-
-#print  g
-#print h
-p = {"action":"install","input":{"vEdgeVPN":0,"vSmartVPN":0,
+		system_ip.append(i['local-system-ip'])      #Filtering System IP and Device ID from the data fetched 
+		device_id.append(i['uuid'])             # and appending it to the list 
+							#this can be further modifed to filter based on existing software version
+	
+payload = {"action":"install","input":{"vEdgeVPN":0,"vSmartVPN":0,
             "data":[{"family":"vedge-mips","version":"18.3.0"}],"versionType":"vmanage","reboot":True,"sync":True},
-            "devices":[{"deviceIP":" ","deviceId":''}],"deviceType":"vedge"}
+            "devices":[{"deviceIP":" ","deviceId":''}],"deviceType":"vedge"} . # Payload to be used for the post call family has to be changed for different device types
 
-urv = 'https://10.195.168.110:443/dataservice/device/action/install'
+urv = 'https://'+ IP + '/dataservice/device/action/install' .   # API Call for Software Upgrade
+
 headers={'Content-Type': 'application/json'}
-#response_post = s.post(uri, auth=HTTPBasicAuth('deepesh', 'deepesh4321!'), data = payload, verify=False)
+
 for (i,j) in zip(g,h):
-	p['devices'][0]['deviceId'] = i
-	p['devices'][0]['deviceIP'] = j
-	q = json.dumps(p)
-	response_post = s.post(urv, auth=HTTPBasicAuth('deepesh', 'deepesh4321!'), data = q, headers=headers, verify=False)
-	print response_post
-
-
-#print e
-#urv = 'https://10.195.168.110:443/dataservice/device/action/install'
-
-#payload = {"action":"install","input":{"vEdgeVPN":0,"vSmartVPN":0,
-	#		"data":[{"family":"vedge-mips","version":"18.3.0"}],"versionType":"vmanage","reboot":true,"sync":true},
-	#		"devices":[{"deviceIP":"1.1.1.4","deviceId":"11OD152130127"}],"deviceType":"vedge"}
-
-#response_post = s.get(uri, auth=HTTPBasicAuth('deepesh', 'deepesh4321!'), data = payload, verify=False)
-#for j,k in e.items():
-#	print j
-#	print k
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-#{"action":"install","input":{"vEdgeVPN":0,"vSmartVPN":0,"data":[{"family":"vedge-x86","version":"18.3.0"}],"versionType":"vmanage","reboot":true,"sync":true},
-#"devices":[{"deviceIP":"1.1.1.215","deviceId":"210a74a7-9a0f-462c-8098-ba60369733ac"}],"deviceType":"vedge"}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#def a(str):
-#	for i in range(0,len(str)):
-#		if str[i].isalpha():
-#			if str[i-1] != '+' or str[i+1] != '+':
-#				return 'false'
-#	return 'True'
-#
-#print a("+a+b+c+")
+	payload['devices'][0]['deviceId'] = i   # . Updating values for device Id and 
+	payload['devices'][0]['deviceIP'] = j	# .    device IP
+	json_payload = json.dumps(payload)    
+	response_post = s.post(urv, auth=HTTPBasicAuth(username, password), data = json_payload, headers=headers, verify=False)
+	print response_post .   #Prints the response code for calls made for all iterations
