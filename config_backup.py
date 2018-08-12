b568a4ab4e7f3e5c3e5be2f1629fcc23a75f4239
Deepesh-Kumar <42011384+Deepesh-Kumar@users.noreply.github.com>
2018-08-12 10:41:18 -0700

    Update config_backup.py

--- config_backup.py
+++ config_backup.py
@@ -3,15 +3,21 @@ import pexpect
 import sys
 from datetime import date
 
-z = 'Green88!!'
+IP = raw_input("Enter the IP of the vmanage: ")
+username = raw_input("Enter the username: ")
+password = raw_input("Enter the password: ")
 
 
-t = date.today()
+backup_server_IP = raw_input("Enter the IP of the backup server: ")
+backup_server_username = raw_input("Enter the username for the backup server: ")
+backup_server_Password = raw_input("Enter the password for the backup server: ")
+
+today_date = date.today()
 
 
 
 #try:
-c = pexpect.spawn('ssh admin@34.203.116.253')
+c = pexpect.spawn('ssh admin@%s' %IP)
 c.timeout = 4
 c.expect("password: ")
 c.logfile = sys.stdout
@@ -19,49 +25,28 @@ c.logfile = sys.stdout
 #	raise Exception("Fail")
 
 
-c.sendline(z)
+c.sendline(password)
 c.expect('#')
-c.sendline('request nms configuration-db backup path /home/admin/%s' %t )
+c.sendline('request nms configuration-db backup path /home/admin/%s' %today_date )
 c.timeout = 1000
 c.expect('#')
-c.sendline('request execute vpn 512 scp %s.tar.gz me@54.201.156.238:/home/me' %t)
+c.sendline('request execute vpn 512 scp %s.tar.gz me@%s:/home/%s' (%today_date, %backup_server_IP) )
 i = c.expect(['yes/no ', 'password:'])
 if i == 0:
 	c.sendline('yes')
 	c.timeout  = 100
 	c.expect('password: ')
-	c.sendline('vtac123')
+	c.sendline(backup_server_Password)
 	c.timeout  = 1000
 	c.expect('#')
 elif i == 1:
-	c.sendline('vtac123')
+	c.sendline(backup_server_Password)
 	c.timeout  = 1000
 	c.expect('#')
 c.sendline('vshell')
 c.expect('$')
-c.sendline('rm - rf %s.tar.gz' %t)
+c.sendline('rm - rf %s.tar.gz' %today_date)
 c.timeout = 100
 c.expect('$')
 c.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
